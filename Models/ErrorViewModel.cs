b9457105553ac96e52d2d56483eebf200f017da8
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2025-04-16 11:15:25 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgriConnect_St10258400_Erin_PROG7311.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
