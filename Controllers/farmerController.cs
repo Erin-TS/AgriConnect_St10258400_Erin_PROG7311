32823c58725445cb68007bf2f00469abc3be1c79
Erin Tyla Steenveld <st10258400@vcconnect.edu.za>
2025-05-13 12:30:24 +0200

    Updated farmer controller to use service to add product

--- Controllers/farmerController.cs
+++ Controllers/farmerController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AgriConnect_St10258400_Erin_PROG7311.Models;
+using AgriConnect_St10258400_Erin_PROG7311.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AgriConnect_St10258400_Erin_PROG7311.Controllers
 {
     public class farmerController : Controller
     {
+        private readonly IFarmerService _farmerService;
+
         public IActionResult addProducts()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult addProduct()
+        {
+           var role = HttpContext.Session.GetString("UserRole");
+            if (role != "Farmer")
+            {
+                return RedirectToAction("UnAuthorisedAccess", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> addProduct(ProductModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                var (success, productName) = await _farmerService.addProductAsync(product);
+                if (success)
+                {
+                    TempData["SuccessMessage"] = $"Product({productName}) added successfully!";
+                    return RedirectToAction("addProducts");
+                }
+                else if (!success)
+                {
+                    ModelState.AddModelError(product.productName, productName);
+                    return View(product);
+                }
+               
+            }
+            else
+            {
+                ModelState.AddModelError("", "Failed to add product. Please try again.");
+            }
+            return View(product);
         }
 
         public IActionResult productListings()
